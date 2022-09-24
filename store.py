e09d3ab2395017df507ff5e3735985687c311160
shruthi-tuplur <113492799+shruthi-tuplur@users.noreply.github.com>
2022-09-24 00:07:38 -0700

    created function to delete last item on list, text box to view cart total now automatically updates with each addition and deletion from shopping cart

--- store.py
+++ store.py
@@ -3,23 +3,20 @@ from tkinter import filedialog
 from turtle import clear, position
 from tkinter import font 
 
-font_tuple = ('Monospace', 10)
 
+#setting up the window of the overall widget
 root = Tk()
 root.configure(background = 'pink', width = 1000, height = 1000)
 root.geometry("500x800")
 
+#initializing variables to find sum of items as they are added into the cart
 total = 0
-item_count  = []
+book_count = 0
+bag_count = 0
+cat_count = 0
 
-def cart_cleared():
-    global total
-    global item_count
-    shoppingCart.delete('1.00', END)
-    orderTotal.delete('1.00', END)
-    item_count = []
-    total = 0
-    
+
+#importing background images for item buttons
 cat_image = PhotoImage(file = r"/Users/shruthituplur/cat.png")
 cat_image_resized = cat_image.subsample(10, 10)
 
@@ -29,83 +26,144 @@ bag_pic_resized = bag_pic.subsample(17, 18)
 book_pic = PhotoImage(file = "/Users/shruthituplur/book.png")
 book_pic_resized = book_pic.subsample(16, 16)
 
+ 
+#creating item buttons and placing them in the widget    
 book = Button(root, text = 'book', fg = 'black', image = book_pic_resized, command = lambda: click('book'))
 book.grid(row = 5, column = 0)
+
 bag = Button(root, text = 'bag', fg = 'black', image = bag_pic_resized, command = lambda: click('bag'))
 bag.grid(row = 5, column = 1)
-cat = Button(root, font = font_tuple, text = 'cat', image = cat_image_resized, fg = 'black', command = lambda: click('cat'))
+
+cat = Button(root, text = 'kitty', image = cat_image_resized, fg = 'black', command = lambda: click('kitty'))
 cat.grid(row = 5, column = 2)
 
-clearButton = Button(root, text = 'clear cart', fg = '#AA336A', command = cart_cleared)
-clearButton.grid(row = 16, column = 1)
 
+
+        
+        
+  
+#creating spacers for aesthetic adjustments
 space = Label(root, text = '', fg = 'pink', background = 'pink' )
 space.grid(row = 8, column = 1)
 space2 = Label(root, text = '', fg = 'pink', background = 'pink' )
 space2.grid(row = 10, column = 1)
 space3 = Label(root, text = '', fg = 'pink', background = 'pink' )
-space3.grid(row = 12, column = 1)
+space3.grid(row = 13, column = 1)
 space4 = Label(root, text = '', fg = 'pink', background = 'pink' )
-space4.grid(row = 15, column = 1)
+space4.grid(row = 16, column = 1)
 space5 =  Label(root, text = '', fg = 'pink', background = 'pink' )
 space5.grid(row = 1, column = 1)
-viewCart = Label(root, text = 'shopping cart', fg = 'white', background = '#AA336A')
-viewCart.grid(row = 9, column = 1)
 
 
-label1 = Label(root, text = 'book - $100', background = '#AA336A', fg = 'white', width = 10)
-label1.grid(row = 6, column = 0)
-label2 = Label(root, text = 'bag - $200', background = '#AA336A', fg = 'white', width = 10)
-label2.grid(row = 6, column = 1)
-labelCat = Label(root, text = 'cat - $400', background = '#AA336A', fg = 'white', width = 10)
-labelCat.grid(row = 6, column = 2)
+#setting up and placing labels
+view_cart_label = Label(root, text = 'shopping cart', fg = 'white', background = '#AA336A')
+view_cart_label.grid(row = 9, column = 1)
+
+
+book_label = Label(root, text = 'book - $100', background = '#AA336A', fg = 'white', width = 10)
+book_label.grid(row = 6, column = 0)
+
+bag_label = Label(root, text = 'bag - $200', background = '#AA336A', fg = 'white', width = 10)
+bag_label.grid(row = 6, column = 1)
+
+cat_label = Label(root, text = 'kitty - $400', background = '#AA336A', fg = 'white', width = 10)
+cat_label.grid(row = 6, column = 2)
 
 storeName = Label(root, text = "shruthi's store", background = '#AA336A', fg = 'white', width = 10, padx = 100)
 storeName.grid(row = 0, column = 1)
 
+#creating text boxes so user can view their shopping cart and their order total
 shoppingCart = Text(root, background = 'white', fg = 'black', width = 20)
 shoppingCart.grid(row = 11, column = 1)
+
 orderTotal = Text(root, background = 'white', fg = 'black', height = 10, width = 40)
-orderTotal.grid(row = 14, column = 1) 
+orderTotal.grid(row = 16, column = 1) 
+
+#setting up command, button, and placement for button that allows user to see their total          
+def view_total(): 
+    global item_count
+    global total
+    orderTotal.delete('1.00', END)
+    item_count  = book_count + bag_count + cat_count
+
+    purchaseStringBook = '\nBooks purchased: %i ' % book_count
     
+    purchaseStringBag = '\nBags purchased: %i ' % bag_count
+    
+    purchaseStringCat = '\nCats purchased: %i ' % cat_count   
+    totalString = '\n\nYour total is: $%i' % total  
+    
+    orderTotal.insert('1.00', totalString)
+    orderTotal.insert('1.00', purchaseStringBook)  
+    orderTotal.insert('1.00', purchaseStringBag)   
+    orderTotal.insert('1.00', purchaseStringCat) 
+        
+         
+ 
+viewTotalLabel = Label(root, text = 'cart total', fg = 'pink', background = '#AA336A')
+viewTotalLabel.grid(row = 15, column = 1)
+    
+#setting up delete button so user can delete the last item placed in their cart
+
+def delete_item():
+    global total
+    global item_count
+    global book_count
+    global bag_count
+    global cat_count
+    last_item = shoppingCart.get('end-2l', "end-1l")
+    if len(last_item) == 5:
+        total -= 100
+        book_count = book_count - 1
+    if len(last_item) == 4:
+        total -= 200
+        bag_count -= 1 
+    if len(last_item) == 6:
+        total -= 400
+        cat_count -= 1
+    shoppingCart.delete("end-2l", "end-1l") 
+    view_total()
+             
+           
+   
+    
+delete_button = Button(root, text = 'delete last item', fg = '#AA336A', command = delete_item)
+delete_button.grid(row = 12, column = 1)    
+
+#setting up command and button so user can clear their cart
+def cart_cleared():
+    global total
+    global item_count
+    shoppingCart.delete('1.00', END)
+    orderTotal.delete('1.00', END)
+    item_count = []
+    total = 0
+ 
+clearButton = Button(root, text = 'clear cart', fg = '#AA336A', command = cart_cleared)
+clearButton.grid(row = 17, column = 1)
+
+#command for item buttons
 def click(item):
     global shoppingCart
     global total
+    global book_count
+    global bag_count
+    global cat_count
     if item == 'book':
         total += 100
-        item_count.append(item)  
+        book_count += 1 
         shoppingCart.insert(END, item + '\n')
     
     elif item == 'bag': 
         total += 200
-        item_count.append(item)  
+        bag_count += 1
         shoppingCart.insert(END, item + '\n')  
         
-    elif item == 'cat': 
+    elif item == 'kitty': 
         total += 400
-        item_count.append(item)  
+        cat_count += 1
         shoppingCart.insert(END, item + '\n')  
-        
-        
-        
-def view_total(): 
-    global item_count
-    global total
-    book_count = item_count.count('book')
-    purchaseStringBook = '\nBooks purchased: %i ' % book_count
-    bag_count = item_count.count('bag')
-    purchaseStringBag = '\nBags purchased: %i ' % bag_count
-    cat_count = item_count.count('cat')
-    purchaseStringCat = '\nCats purchased: %i ' % cat_count   
-    totalString = '\n\nYour total is: $%i' % total  
-    orderTotal.insert('1.00', totalString)
-    orderTotal.insert('1.00', purchaseStringBook)  
-    orderTotal.insert('1.00', purchaseStringBag)   
-    orderTotal.insert('1.00', purchaseStringCat)  
-        
-         
- 
-viewTotal = Button(root, text = 'view cart total', fg = '#AA336A', background = '#AA336A', command = view_total)
-viewTotal.grid(row = 13, column = 1)
-    
+    view_total()    
+
+
 root.mainloop()
